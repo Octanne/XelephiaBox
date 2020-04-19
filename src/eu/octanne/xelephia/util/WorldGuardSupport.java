49c0a403a536463206ef9640ec4d34df2dfb64cd
Octanne <contact@octanne.eu>
2020-04-19 19:25:48 +0200

    change wrong argt for the correct in getMethod => testState

--- src/eu/octanne/xelephia/util/WorldGuardSupport.java
+++ src/eu/octanne/xelephia/util/WorldGuardSupport.java
@@ -2,6 +2,7 @@ package eu.octanne.xelephia.util;
 
 import java.lang.reflect.InvocationTargetException;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -14,33 +15,37 @@ import com.sk89q.worldguard.protection.flags.DefaultFlag;
 public class WorldGuardSupport {
 	@SuppressWarnings("rawtypes")
 	static public boolean isPvPActive(Entity e) {
-		Entity p = e;
-		if(((Projectile)p).getShooter() instanceof Player) {
-			p = (Player)((Projectile)e).getShooter();
-		}else if(!(p instanceof Player)) return false;
-		
-		try {
-			Class<?> classWorldGuardPlugin = Class.forName("com.sk89q.worldguard.bukkit.WorldGuardPlugin");
-			Class<?> classRegionContainer = Class.forName("com.sk89q.worldguard.bukkit.RegionContainer");
-			Class<?> classRegionQuery = Class.forName("com.sk89q.worldguard.bukkit.RegionQuery");
-			Class<?> classLocalPlayer = Class.forName("com.sk89q.worldguard.LocalPlayer");
-			Class<?> classDefaultFlag = Class.forName("com.sk89q.worldguard.protection.flags.DefaultFlag");
-			
-			Object worldGuardPlugin = classWorldGuardPlugin.getMethod("inst").invoke(null);
-			Object regionContainer = classWorldGuardPlugin.getMethod("getRegionContainer").invoke(worldGuardPlugin);
-			Object query = classRegionContainer.getMethod("createQuery").invoke(regionContainer);
-			
-			Class[] wrapPara = {Player.class};
-			Object wrapPlayer = classWorldGuardPlugin.getMethod("wrapPlayer", wrapPara).invoke(worldGuardPlugin, e);
-			
-			Object pvpFlag = classDefaultFlag.getMethod("PVP").invoke(null);
-			
-			Class[] queryPara = {Location.class, classLocalPlayer, classDefaultFlag};
-			return ((boolean)classRegionQuery.getMethod("testState", queryPara).invoke(query, e.getLocation(), wrapPlayer, pvpFlag));
-		} catch (ClassNotFoundException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e1) {
-			e1.printStackTrace();
-			//return false;
-		}
+		if(Bukkit.getPluginManager().isPluginEnabled("WorldGuard")) {
+			Player p;
+			if(e instanceof Projectile && (((Projectile)e).getShooter()) instanceof Player) {
+				p = (Player)((Projectile)e).getShooter();
+			}else if(!(e instanceof Player)) return false;
+			p = (Player)e;
+			try {
+				Class<?> classWorldGuardPlugin = Class.forName("com.sk89q.worldguard.bukkit.WorldGuardPlugin");
+				Class<?> classRegionContainer = Class.forName("com.sk89q.worldguard.bukkit.RegionContainer");
+				Class<?> classRegionQuery = Class.forName("com.sk89q.worldguard.bukkit.RegionQuery");
+				Class<?> classLocalPlayer = Class.forName("com.sk89q.worldguard.LocalPlayer");
+				Class<?> classDefaultFlag = Class.forName("com.sk89q.worldguard.protection.flags.DefaultFlag");
+				Class<?> classRegionAssociable = Class.forName("com.sk89q.worldguard.protection.association.RegionAssociable");
+				
+				Object worldGuardPlugin = classWorldGuardPlugin.getMethod("inst").invoke(null);
+				Object regionContainer = classWorldGuardPlugin.getMethod("getRegionContainer").invoke(worldGuardPlugin);
+				Object query = classRegionContainer.getMethod("createQuery").invoke(regionContainer);
+				
+				Class[] wrapPara = {Player.class};
+				Object wrapPlayer = classWorldGuardPlugin.getMethod("wrapPlayer", wrapPara).invoke(worldGuardPlugin, p);
+				
+				Object pvpFlag = classDefaultFlag.getField("PVP");
+				
+				Class[] queryPara = {Location.class, classLocalPlayer, classRegionAssociable};
+				return ((boolean)classRegionQuery.getMethod("testState", queryPara).invoke(query, p.getLocation(), wrapPlayer, pvpFlag));
+			} catch (ClassNotFoundException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException | NoSuchFieldException e1) {
+				Bukkit.getLogger().info("[Xelephia] Error in WorldGuard Support System");
+				e1.printStackTrace();
+				return true;
+			}
+		}//else return true;
 		
 		if(e instanceof Player) {
 			RegionQuery query = WorldGuardPlugin.inst().getRegionContainer().createQuery();
