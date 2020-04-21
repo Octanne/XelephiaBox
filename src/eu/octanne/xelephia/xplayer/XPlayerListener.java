33a1b77e660d7965556417992ef88ac97e401c93
Octanne <contact@octanne.eu>
2020-04-21 03:19:40 +0200

    repair add health

--- src/eu/octanne/xelephia/xplayer/XPlayerListener.java
+++ src/eu/octanne/xelephia/xplayer/XPlayerListener.java
@@ -224,7 +224,7 @@ public class XPlayerListener implements Listener {
 		// Killer
 		if(e.getEntity().getKiller() != null) {
 			XPlayer xPKiller = XelephiaPlugin.getXPlayer(e.getEntity().getKiller().getUniqueId());
-			xPKiller.getBukkitPlayer().setHealth(xPKiller.getBukkitPlayer().getHealth()+6);
+			xPKiller.getBukkitPlayer().setHealth(xPKiller.getBukkitPlayer().getHealth()+6 < 20 ? xPKiller.getBukkitPlayer().getHealth()+6 : 20);
 			xPKiller.actualKillStreak++;
 			xPKiller.killCount++;
 			if(xPKiller.getBukkitPlayer().getInventory().firstEmpty() == -1) xPKiller.getBukkitPlayer().getWorld()
